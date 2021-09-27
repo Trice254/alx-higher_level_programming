80c2e98eb64846d2081b27074faec348f6058baa
Trice254 <tricemegamind254@gmail.com>
2021-09-27 12:28:37 -0700

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ divides 2 integers and prints the result.
+    """
+
+
+def safe_print_division(a, b):
+    try:
+        x = a / b
+    except:
+        x = None
+    finally:
+        print("Inside result: {}".format(x))
+        return x
+    
