b353413fe5ad99a7d291f0b51d4ae7965a0e87e5
Trice254 <tricemegamind254@gmail.com>
2021-10-11 11:32:28 -0700

    ask a question romm really works

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """
-Contains parent class BaseGeometry
-with public instance method area and integer_validation
-Contains subclass Rectangle
-with instantiation of private attributes width and height, validated by parent,
-extends parent's area method and prints with __str__
-Contains subclass Square
-with instantiation of private attribute size, validated by superclass
+    Square Module
 """
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """inherits from Rectangle, who inherits from BaseGeometry
-    methods:
-        __init__(self, size)
+    """
+        Square Class
     """
     def __init__(self, size):
-        """initializes size
-        Args:
-            size (int): private
         """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+            Initialize the square base on Rectangle
+        """
         self.__size = size
-        
+        self.integer_validator('size', self.__size)
+
+    def area(self):
+        """
+        define area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+            __str__
+        """
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+    
