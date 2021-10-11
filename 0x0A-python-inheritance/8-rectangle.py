b291f0d955551d13e2ccb71d1ef22f4cacedc3d7
Trice254 <tricemegamind254@gmail.com>
2021-10-11 11:27:53 -0700

    making changes after cher

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """
-Contains parent class BaseGeometry
-with public instance method area and integer_validator
-Contains subclass Rectangle
-with instantiation of private attributes width and height, validated by parent
+    Rectangle module
 """
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """inherits from BaseGeometry
-    Methods:
-        __init__(self, width, height)
+    """
+    class  that inherits from BaseGeometry
     """
     def __init__(self, width, height):
-        """validate and initialize width and height
-        Args:
-            width (int): private
-            height (int): private
         """
-        super().integer_validator("width", width)
+            Initialize rectangle from BaseGeometry
+        """
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
         
